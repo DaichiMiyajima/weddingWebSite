e0902bf7a2ff8d7e3de140b34f68025acbe21db6
DaichiMiyajima <let_it_be7daichi@yahoo.co.jp>
2015-10-24 00:21:08 +0900

    test for iPhone

---
+++ public/javascripts/makeown.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+	$.ajax({
+    	url  : "/groupchat",
+    	type : "get",
+		async:false,
+    	success : function(responses) {
+		    _delete_element("reply_comment_rireki_article");
+			var article = document.querySelector('#reply_comment_rireki_article');
+			var li;
+			for (var i=0 ; i < responses.length ; i++){
+			  h3 = document.createElement("h3");
+			  var section = document.createElement("section");
+			  var p = document.createElement("p");
+			  h3.textContent = responses[i].userName;
+			  p.textContent = responses[i].groupChat;
+			  article.appendChild(h3);
+			  article.appendChild(section);
+			  section.appendChild(p);
+			}
+			
+    	},
+    	error : function(){
+        
+    	}
+	});	
+});
+	
+function kakunin() {
+                                            
+    swal({   
+        title: "Invitation!",   
+        text: "Put the below button or Write your information:",   
+        type: "input",   
+        showCancelButton: true,   
+        closeOnConfirm: false,   
+        animation: "slide-from-top",   
+        inputPlaceholder: "Your Name" ,
+        }, function(inputValue){ 
+                if (inputValue === false) return false; 
+                if (inputValue === "") {
+                         swal.showInputError("You need to write something!");     
+                         return false  
+                }      
+                swal("Nice!", "You wrote: " + inputValue, "success"); 
+        });
+};
+
