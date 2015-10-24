e885c598a7406d13e85866667d3f3464c040dcf2
DaichiMiyajima <let_it_be7daichi@yahoo.co.jp>
2015-10-25 00:19:18 +0900

    test

--- public/javascripts/makeown.js
+++ public/javascripts/makeown.js
@@ -1,42 +1,5 @@
 $(document).ready(function() {
-	$('#myContainer').multiscroll({
-            	sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE'],
-            	anchors: ['first', 'second', 'third'],
-            	menu: '#menu',
-            	navigation: true,
-            	navigationTooltips: ['One', 'Two', 'Three'],
-            	loopBottom: true,
-            	loopTop: true
-            });
 });
-	
-function kakunin() {
-    
-    swal({   
-        title: "HTML <small>Title</small>!",   
-        text: "<textarea type=\"text\" tabIndex=\"3\" class=\"input-text\"  style=\"font-size: 16px !important;\"></textarea> \n",   
-        html: true,
-        animation: false
-    });
-                                        
-    /*swal({   
-        title: "Invitation!",   
-        text: "Put the below button or Write your information:",   
-        type: "input",   
-        showCancelButton: true,   
-        closeOnConfirm: false,   
-        animation: "slide-from-top",   
-        inputPlaceholder: "Your Name" ,
-        }, function(inputValue){ 
-                if (inputValue === false) return false; 
-                if (inputValue === "") {
-                         swal.showInputError("You need to write something!");     
-                         return false  
-                }      
-                swal("Nice!", "You wrote: " + inputValue, "success"); 
-        });*/
-};
-
 
 $("#appform").animatedModal({
                 	      modalTarget:'modal-02',
