c94405d17374c6b8b965fd45ed3d403a2ce7bf93
DaichiMiyajima <let_it_be7daichi@yahoo.co.jp>
2015-11-26 00:16:18 +0900

    a little change
    delete console.log

--- routes/facebook.js
+++ routes/facebook.js
@@ -4,12 +4,6 @@ var mysql = require(__dirname + '/../lib/mysql.js');
 
 /* GET home page. */
 router.post('/facebook', function(req, res, next) {
-console.log(req.body.formAttendance);
-console.log(req.body.formFriends);
-console.log(req.body.formName);
-console.log(req.body.formEmail);
-console.log(req.body.picture);
-console.log(req.body.formMessage);
 
 mysql.insertAttendance(req.body.formAttendance,req.body.formFriends,req.body.formName,req.body.formEmail,'',req.body.formMessage,req.body.picture,req.body.formFacebookId);
     res.render('index');
